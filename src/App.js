6ead074dbcb598d5b8eaf0c7b8f959945b687293
Abuzar Ali <abuzaralixwave@gmail.com>
2024-11-25 09:38:24 +0500

    React Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
